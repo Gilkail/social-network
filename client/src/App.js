db9b95784b3ee2e0ffcf53887b2263b23d8def2a
Gil Katchko <gil.katchko@gmail.com>
2019-02-25 15:29:04 +0200

    Added posts and comments

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,8 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from './components/profile/Profile';
 import NotFound from "./components/not-found/NotFound";
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -60,6 +62,8 @@ class App extends Component {
                 <PrivateRoute exact path="/edit-profile" component={EditProfile} />
                 <PrivateRoute exact path="/add-experience" component={AddExperience} />
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
+                <PrivateRoute exact path="/feed" component={Posts} />
+                <PrivateRoute exact path="/post/:id" component={Post} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
